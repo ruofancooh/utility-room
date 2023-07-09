c037ae27cb2838f6d35f2e0e9651ae78f4babb0a
rcooh <rcooh@outlook.com>
2023-07-09 15:07:39 +0800

    commit

--- mysql.py
+++ mysql.py
@@ -2,7 +2,7 @@ import pymysql
 
 #打开数据库连接
 db = pymysql.connect(host='a.com',
-                     port=3306
+                     port=3306,
                      user='root',
                      password='******',
                      database='db1')
@@ -10,30 +10,32 @@ db = pymysql.connect(host='a.com',
 #使用cursor()方法创建一个游标对象
 cursor = db.cursor()
 
+
 def select_all():
     sql = 'select * from department'
-    
+
     #使用execute()方法执行SQL
     cursor.execute(sql)
-    
+
     #获取所有记录并打印
     results = cursor.fetchall()
     for i in results:
         print(i)
     print()
 
+
 select_all()
 
 sql = '''INSERT INTO db1.department (DEPT_NO,DEPT_NAME)
 	    VALUES ('05','外语系');'''
 try:
-   #执行SQL语句
-   cursor.execute(sql)
-   #提交到数据库执行
-   db.commit()
+    #执行SQL语句
+    cursor.execute(sql)
+    #提交到数据库执行
+    db.commit()
 except:
-   #发生错误时回滚
-   db.rollback()
+    #发生错误时回滚
+    db.rollback()
 
 select_all()
 
